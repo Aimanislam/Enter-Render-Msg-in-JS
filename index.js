8839963e854708e540f394979737940c3d362b4e
Aimanislam <57367836+Aimanislam@users.noreply.github.com>
2022-01-24 19:43:23 +0500

    Render message file added

---
+++ index.js
@@ -0,0 +1,7 @@
+//Enter Render Message
+
+function purchase(){
+let errorParagraph = document.getElementById("error")
+// document.getelementbyid is a selector
+errorParagraph.innerHTML = "Something went wrong, please try again"
+}
